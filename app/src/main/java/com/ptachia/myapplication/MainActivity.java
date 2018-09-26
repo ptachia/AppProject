6994ff2005dece7753e738e492357d2ac436b9f1
ptachia <ptachia.rechel@gmail.com>
2018-09-26 22:17:11 +0300

    26/9/18

--- app/src/main/java/com/ptachia/myapplication/MainActivity.java
+++ app/src/main/java/com/ptachia/myapplication/MainActivity.java
@@ -1,13 +1,15 @@
 package com.ptachia.myapplication;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     Button area1, area2, deepness1, deepness2, cold1, cold2, level1, level2;
-
+    Fragment areaF, deepnessF, coldF, levelF;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
         cold2 = findViewById(R.id.cold2);
         level1 = findViewById(R.id.level1);
         level2 = findViewById(R.id.level2);
+
+        area1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                areaF = new AreaScreen();
+                android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().add(R.id.main_fragment, areaF).addToBackStack(null).commit();
+            }
+        });
     }
 }
